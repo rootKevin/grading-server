3e97980aa8c2fdc9296811c3564180c86b485e74
rootKevin <superbordii@gmail.com>
2026-01-21 17:00:57 +0900

    0.0.0.0

--- server.js
+++ server.js
@@ -164,4 +164,6 @@ app.post("/login", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("🚀 Server running on", PORT));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("🚀 Server running on", PORT);
+});
